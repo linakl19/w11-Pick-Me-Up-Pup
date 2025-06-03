70b7aff9470bbf11990d527b3918d88e16b108f3
Lina Martinez <lmcastro3501@gmail.com>
2025-06-03 08:47:55 -0700

    Add a new dog when a button is clicked.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ console.log('loaded!');
 
 const NUMBER_OF_DOGS = 15;
 const state = {
-  dogImg: null
+  dogImg: null,
+  addDogButton: null,
+  dogImageContainer: null,
 };
 
 const pickRandomNumber = () => {
@@ -13,15 +15,35 @@ const setRandomDog = () => {
   const imgNumber = pickRandomNumber();
   const newImgName = `../imgs/${imgNumber}.jpeg`;
   state.dogImg.src = newImgName;
+};
+
+const addNewDog = (event) => {
+  // Pick a random rumber
+  // set the path for the new dogImg
+  // Create a img element
+  // Add the src to the img
+  // Add the alt to the new img
+  const imgNumber = pickRandomNumber();
+  const newImgName = `../imgs/${imgNumber}.jpeg`;
+  const newDog = document.createElement('img');
+  newDog.src = newImgName;
+  newDog.alt = `Random dog number ${imgNumber}`;
+  state.dogImageContainer.prepend(newDog);
 }
+const registerEvents = () => {
+  state.addDogButton.addEventListener('click', addNewDog);
+};
 
 const onLoaded = () => {
-  loadControls();
-  setRandomDog();
+  loadControls(); // get the image from the page
+  setRandomDog(); // set a new image source
+  registerEvents();
 };
 
 const loadControls = () => {
   state.dogImg = document.getElementById('dog-image');
+  state.addDogButton = document.getElementById('addDogButton');
+  state.dogImageContainer = document.getElementById('dog-img-container');
 };
 
 onLoaded();
