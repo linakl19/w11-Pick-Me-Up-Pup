4f6558771b0e4f75059ebbd1c52cc5bcbd48804b
Lina Martinez <lmcastro3501@gmail.com>
2025-06-03 08:00:37 -0700

    Display random image. First Version. NO state

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('loaded!');
+
+const NUMBER_OF_DOGS = 15;
+
+const pickRandomNumber = () => {
+  return Math.floor(Math.random() * NUMBER_OF_DOGS) + 1;
+};
+
+const onLoaded = () => {
+  const dogImg = document.getElementById('dog-image');
+  const imgNumber = pickRandomNumber();
+  const newImgName = `../imgs/${imgNumber}.jpeg`;
+  dogImg.src = newImgName;
+};
+
+onLoaded();
